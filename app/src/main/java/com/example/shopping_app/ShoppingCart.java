06eb71e851471ab27da477dfff646fc0d05f6a24
sergi-s <52626795+sergi-s@users.noreply.github.com>
2021-05-27 18:47:08 +0200

    Total Price

--- app/src/main/java/com/example/shopping_app/ShoppingCart.java
+++ app/src/main/java/com/example/shopping_app/ShoppingCart.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -16,11 +17,11 @@ import java.util.List;
 
 public class ShoppingCart extends AppCompatActivity {
     RecyclerView recycleView;
-    List<Item> list = new ArrayList<Item>();
-    int images[] = {R.drawable.knee_pads, R.drawable.helmet, R.drawable.pants, R.drawable.bicycles, R.drawable.bottles, R.drawable.shirts, R.drawable.nikes};
     List<Item> order;
     double sum = 0;
+    TextView totalprice;
     Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +29,8 @@ public class ShoppingCart extends AppCompatActivity {
 
         Intent intent = getIntent();
         order = (List<Item>) intent.getSerializableExtra("List");
-        btn=findViewById(R.id.button);
-        recycleView=findViewById(R.id.shoppingCartView);
+        btn = findViewById(R.id.button);
+        recycleView = findViewById(R.id.shoppingCartView);
 
         RecycleViewAdapterCart myAdapter = new RecycleViewAdapterCart(this, order);
 
@@ -37,31 +38,30 @@ public class ShoppingCart extends AppCompatActivity {
         recycleView.setLayoutManager(new LinearLayoutManager(this));
 
 
-//        for (int i = 0; i < order.size(); i++) {
-//            Log.d("Ordred", String.valueOf(order.get(0)));
-//        }
-
         for (int i = 0; i < order.size(); i++) {
             String str = order.get(i).Price;
             StringBuilder sb = new StringBuilder(str);
             sum += Double.parseDouble(sb.deleteCharAt(str.length() - 1).toString());
         }
 
-        Log.d("Total Price", String.valueOf(sum));
+        totalprice = (TextView) findViewById(R.id.totalprice);
+        totalprice.setText( "Total Price "+String.valueOf(sum));
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
-                intent.putExtra("total",sum);
+                intent.putExtra("total", sum);
 
                 intent.setAction("com.example.shopping_app.CUSTOM_INTENT");
                 sendBroadcast(intent);
-                Log.d("aaaaaaaaaaaaaaaaaaaaaaa", "broadcastIntent: ");
+
+
             }
         });
     }
 
-    public void broadcastIntent(View view){
+    public void broadcastIntent(View view) {
 
     }
 
